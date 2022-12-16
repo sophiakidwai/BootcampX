41d2b71c7ab253a61ba21c61620fd89f42e1531d
sophia kidawai <sophia.kidwai@telus.com>
2022-12-15 16:01:51 -0800

    adding students

---
+++ bootcamp_app/students.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'T882382',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const args = process.argv.slice(2)
+const cohortName = args[0]
+const limit = args[1]
+const values = [`%${cohortName}%`, limit]
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+});
